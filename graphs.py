bc7c3424a2c6522242686ec819b8f072f7a48d41
Volcore001 <36526721+Volcore001@users.noreply.github.com>
2021-03-08 19:01:25 -0800

    More work done on the graphs

--- graphs.py
+++ graphs.py
@@ -30,14 +30,15 @@ class Graphs:
     def multi(self, data1, data2, string1, string2):
         concatenated = pd.concat([data1.assign(shows=string1), data2.assign(shows=string2)])
         concatenated['imdb_rating'] = pd.to_numeric(concatenated['imdb_rating'])
-        concatenated1 = concatenated[concatenated['season']==1]    
-        print(concatenated1)
-        fig, axes = plt.subplots(min(data1['season'].max(),data2['season'].max()))
-        plot = sns.regplot(ax = axes[0], x='season', y='imdb_rating', data = concatenated1)
-        #plot = sns.joinplot(data=concatenated,x="season",y="imdb_rating",hue = "shows")
-        #plot.set_yticklabels([0,1,2,3,4,5,6,7,8,9])
+        seasons = min(data1['season'].max(),data2['season'].max())
+        concatenated["episode_num"] = pd.to_numeric(concatenated["episode_num"], downcast="float")
+        fig, axes = plt.subplots(1,seasons)
+        for i in range(seasons):
+            concatenated1 = concatenated[concatenated['season']==i+1]    
+            plot = sns.scatterplot(ax = axes[i], x='episode_num', y='imdb_rating', data = concatenated1,hue="shows")
+            axes[i].set_xlabel('Season ' + str(i+1) + " episodes")
+            axes[i].set_ylabel('IMDB Rating')
         print(concatenated.columns)
-        title = "IMDb ratings of {} vs {}".format(string1.capitalize(), string2.capitalize())
-        plt.title(title, pad = 0.2)
+
         plt.savefig('scatter2.png', bbox_inches = 'tight')
         plt.show()
