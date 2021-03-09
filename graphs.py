f139c871258ccf1b26462d26988d27f1f1540939
Volcore001 <36526721+Volcore001@users.noreply.github.com>
2021-03-09 12:38:24 -0800

    Fixed some bugs

--- graphs.py
+++ graphs.py
@@ -27,21 +27,19 @@ class Graphs:
         plt.title(title, pad=0.2)
         plt.savefig('scatter.png', bbox_inches='tight')
         plt.show()
-        
-        
+
     def boxplot(self, data1, data2, string1, string2):
         concat = pd.concat([data1.assign(shows=string1),
                             data2.assign(shows=string2)])
         concat['imdb_rating'] = pd.to_numeric(concat['imdb_rating'])
         sns.boxplot(x="season", y="imdb_rating", hue="shows",
-                 data=concat, palette="Set3")
+                    data=concat, palette="Set3")
         title = "Comparative Boxplots of IMDb ratings of {} vs {}".format(string1.capitalize(),
-                                                  string2.capitalize())
+                                                                          string2.capitalize())
         plt.title(title)
         plt.savefig('boxplot.png', bbox_inches='tight')
         plt.show()
-        
-        
+
     def multi(self, data1, data2, string1, string2):
         concat = pd.concat([data1.assign(shows=string1),
                             data2.assign(shows=string2)])
@@ -49,25 +47,16 @@ class Graphs:
         seasons = min(data1['season'].max(), data2['season'].max())
         concat["episode_num"] = pd.to_numeric(concat["episode_num"],
                                               downcast="float")
-        axes = plt.subplots(1, seasons)
-        for i in range(seasons):
-            concatenated1 = concat[concat['season'] == i + 1]
-            sns.scatterplot(ax=axes[i], x='episode_num',
-                            y='imdb_rating',
-                            data=concatenated1, hue="shows")
-            axes[i].set_xlabel('Season '+str(i+1)+" episodes")
-            axes[i].set_ylabel('IMDB Rating')
-        #axes = plt.subplots(1, seasons)
         fig, axes = plt.subplots(1, seasons)
         for i in range(seasons):
             concatenated1 = concat[concat['season'] == i + 1]
-            #sns.scatterplot(ax=axes[i], x='episode_num', y='imdb_rating', data=concatenated1, hue="shows")
-            sns.relplot(ax=axes[i], x='episode_num', y='imdb_rating', data=concatenated1, hue="shows")
+            sns.scatterplot(ax=axes[i], x='episode_num', y='imdb_rating',
+                            data=concatenated1, hue="shows")
             axes[i].set_xlabel('Season '+str(i+1))
             if i == 0:
                 axes[i].set_ylabel('IMDB Rating')
             else:
                 axes[i].set_ylabel('')
-                axes[i].set_yticks([])
+                # axes[i].set_yticks([])
         plt.savefig('scatter2.png', bbox_inches='tight')
         plt.show()
